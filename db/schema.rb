48129130b126d25d0ca9d3a2aaa9382e42a249ed
Tommy A. Caruso <tommy.caruso2118@gmail.com>
2016-01-29 21:02:49 -0500

    Add ability to archive a user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124032403) do
+ActiveRecord::Schema.define(version: 20160130014703) do
 
   create_table "guilds", force: :cascade do |t|
     t.string   "game"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160124032403) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.datetime "archived_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
