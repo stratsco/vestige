2882f48d3c25262a3daddefbea4468b217e679f9
Tommy A. Caruso <tommy.caruso2118@gmail.com>
2016-01-29 21:02:49 -0500

    Admin can archive a user

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
     root "application#index"
 
     resources :guilds, only: [:new, :create, :destroy]
-    resources :users
+    resources :users do
+      member do
+        patch :archive
+      end
+    end
   end
 
   devise_for :users
